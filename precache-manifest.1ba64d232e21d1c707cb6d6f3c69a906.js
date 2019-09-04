dde6d482e3d242e778d86bdce63149ee4fd0dcd1
lorelea <shaynalekohmaher@gmail.com>
2019-09-04 16:26:45 -0700

    Updates

---
+++ precache-manifest.1ba64d232e21d1c707cb6d6f3c69a906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f3242b57cfdf1298510c3c95cb6661",
+    "url": "/react-IEX-lookup/index.html"
+  },
+  {
+    "revision": "6b15ca0cdb32c7657cd6",
+    "url": "/react-IEX-lookup/static/css/main.18efbc50.chunk.css"
+  },
+  {
+    "revision": "d63244a474ed0da907d8",
+    "url": "/react-IEX-lookup/static/js/2.14c353bc.chunk.js"
+  },
+  {
+    "revision": "6b15ca0cdb32c7657cd6",
+    "url": "/react-IEX-lookup/static/js/main.ee4c3a8b.chunk.js"
+  },
+  {
+    "revision": "ff4075e78c8acb9d1eb4",
+    "url": "/react-IEX-lookup/static/js/runtime~main.50c53b4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-IEX-lookup/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-IEX-lookup/precache-manifest.1ba64d232e21d1c707cb6d6f3c69a906.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-IEX-lookup/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
