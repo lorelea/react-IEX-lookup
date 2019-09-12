ee3a249796aaa95d3680576e7c2c3a30aaad8f59
lorelea <shaynalekohmaher@gmail.com>
2019-09-11 22:56:55 -0700

    add keypress listeners to autocomplete suggestion list

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -1,67 +1,123 @@
-import React from 'react';
-import './Autocomplete.scss';
+import React from "react";
+import "./Autocomplete.scss";
 
 class Autocomplete extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            suggestions: [],
-            query: this.props.value || ''
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      suggestions: [],
+      query: "" // this.props.value || ""
+    };
+  }
 
-    handleInputChange = (event) => {
-        const { items } = this.props;
-        const query = event.target.value;
-        let suggestions = [];
-        if (query.length) {
-            const regex = new RegExp(`^${query}`, 'i');
-            suggestions = items.sort().filter(item => regex.test(item));
+  componentWillReceiveProps(newProps) {
+    if (newProps.value !== this.state.query) {
+      // console.log(`newProps.value: ${newProps.value}`);
+      this.setState({ query: newProps.value });
+    }
+  }
+
+  componentDidMount() {
+    this.search.parentNode.addEventListener("keydown", this.handleKeyDown);
+    // window.addEventListener('scroll', this.handleScroll);
+  }
+  componentWillUnmount() {
+    this.search.parentNode.removeEventListener("keydown", this.handleKeyDown);
+    // window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleKeyDown = event => {
+    const activeListElement =
+      this.search.parentNode.querySelector(".active") ||
+      this.search.parentNode.querySelector("li:first-child");
+
+    switch (event.keyCode) {
+      case 13: // enter
+        this.handleSuggestionSelected(activeListElement.innerText);
+        break;
+      case 38: // up
+        if (activeListElement.previousSibling) {
+          activeListElement.classList.remove("active");
+          activeListElement.previousSibling.classList.add("active");
         }
-        this.setState({ suggestions, query });
-    }
-
-    handleClearClick = () => {
-        this.setState({ query: '' });
-        this.search.focus();
-    }
-
-    handleSuggestionSelected = (value) => {
-        this.setState({ 
-            suggestions: [],
-            query: value
-        }, () => {
-            this.props.onValueUpdated && this.props.onValueUpdated(value); 
-        });
-    }
-
-    renderFilteredSuggestions () {
-        const { suggestions } = this.state;
-        if (!suggestions.length) {
-            return null;
+        break;
+      case 40: // down
+        if (activeListElement.nextSibling) {
+          activeListElement.classList.remove("active");
+          activeListElement.nextSibling.classList.add("active");
         }
-        return (
-            <ul>
-                {suggestions.map((item, index) => <li key={item + index} onClick={() => this.handleSuggestionSelected(item)}>{item}</li>)}
-            </ul>
-        );
+        break;
+      default:
+        break;
     }
+  };
 
-    render () {
-        return (
-            <div className={"my-autocomplete " + this.props.className}>
-                <input
-                    type="text"
-                    ref={input => this.search = input}
-                    placeholder={this.props.placeholder}
-                    value={this.state.query}
-                    onChange={this.handleInputChange}
-                />
-                {this.state.query && <span className="clearable" onClick={this.handleClearClick}></span>}
-                {this.renderFilteredSuggestions()}
-            </div>
-        );
+  handleInputChange = event => {
+    const { items } = this.props;
+    const query = event.target.value;
+    let suggestions = [];
+    if (query.length) {
+      const regex = new RegExp(`^${query}`, "i");
+      suggestions = items.sort().filter(item => regex.test(item));
     }
+    this.setState({ suggestions, query });
+  };
+
+  handleClearClick = () => {
+    this.setState({ query: "" });
+    this.search.focus();
+  };
+
+  handleSuggestionSelected = value => {
+    this.setState(
+      {
+        suggestions: [],
+        query: value
+      },
+      () => {
+        this.props.onValueUpdated && this.props.onValueUpdated(value);
+      }
+    );
+  };
+
+  renderFilteredSuggestions() {
+    const { suggestions } = this.state;
+    const activeValue = this.props.value;
+    if (!suggestions.length) {
+      return null;
+    }
+    return (
+      <ul>
+        {suggestions.map((item, index) => (
+          <li
+            className={item === activeValue ? "active" : ""}
+            key={item + index}
+            onClick={() => this.handleSuggestionSelected(item)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div className={"my-autocomplete " + this.props.className}>
+        <input
+          type="text"
+          ref={input => (this.search = input)}
+          placeholder={this.props.placeholder}
+          value={this.state.query}
+          onChange={this.handleInputChange}
+        />
+        {this.state.query && (
+          <span className="clearable" onClick={this.handleClearClick}></span>
+        )}
+        {this.renderFilteredSuggestions()}
+      </div>
+    );
+  }
 }
 
 export default Autocomplete;
--- src/components/IEXInfoLookup.js
+++ src/components/IEXInfoLookup.js
@@ -1,115 +1,130 @@
-import React from 'react';
-import './IEXInfoLookup.scss';
-import WindowedSelect, { createFilter } from 'react-windowed-select';
-import Autocomplete from './Autocomplete';
-import Switch from './Switch'
-import APICall from 'APICall';
+import React from "react";
+import "./IEXInfoLookup.scss";
+import WindowedSelect, { createFilter } from "react-windowed-select";
+import Autocomplete from "./Autocomplete";
+import Switch from "./Switch";
+import APICall from "APICall";
 
 class IEXInfoLookup extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            symbols: [],
-            activeSymbol: '',
-            price: '',
-            company: {},
-            showAutocomplete: false
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      symbols: [],
+      activeSymbol: "",
+      price: "",
+      company: {},
+      showAutocomplete: false
+    };
+  }
 
-    getAllSymbols () {
-        APICall.getAllSymbols()
-            .then(response => {
-                this.setState({
-                    symbols: response
-                });
-            })
-    }
+  getAllSymbols() {
+    APICall.getAllSymbols().then(response => {
+      this.setState({
+        symbols: response
+      });
+    });
+  }
 
-    getLatestPrice () {
-        APICall.getLatestPrice(this.state.activeSymbol)
-            .then(response => {
-                this.setState({
-                    price: response
-                });
-            })
-    }
+  getLatestPrice() {
+    APICall.getLatestPrice(this.state.activeSymbol).then(response => {
+      this.setState({
+        price: response
+      });
+    });
+  }
 
-    getCompanyInfo () {
-        APICall.getCompanyInfo(this.state.activeSymbol)
-            .then(response => {
-                this.setState({
-                    company: response
-                });
-            })
-    }
+  getCompanyInfo() {
+    APICall.getCompanyInfo(this.state.activeSymbol).then(response => {
+      this.setState({
+        company: response
+      });
+    });
+  }
 
-    handleSymbolChanged = (activeSymbol) => {
-        this.setState({ activeSymbol }, () => {
-            this.getLatestPrice();
-            this.getCompanyInfo();
-        });
-    }
+  handleSymbolChanged = activeSymbol => {
+    this.setState({ activeSymbol }, () => {
+      this.getLatestPrice();
+      this.getCompanyInfo();
+    });
+  };
 
-    handleSwitchToggle = (isChecked) => {
-        this.setState({
-            showAutocomplete: isChecked
-        });
-    }
+  handleSwitchToggle = isChecked => {
+    this.setState({
+      showAutocomplete: isChecked
+    });
+  };
 
-    componentDidMount () {
-        this.getAllSymbols();
-    }
+  componentDidMount() {
+    this.getAllSymbols();
+  }
 
-    renderSymbolInfo () {
-        let { symbols, activeSymbol, company: { description } } = this.state;
-        if (!symbols.length || !activeSymbol) {
-            return null;
-        }
-        const regex = new RegExp(`^${activeSymbol}`, 'i');
-        let symbolInfo = symbols.find(item => regex.test(item.symbol));
-        if (!symbolInfo) {
-            return null;
-        }
-        return (
-            <dl>
-                <dt>Symbol</dt>
-                <dd>{activeSymbol}</dd>
-                <dt>Current Stock Price</dt>
-                <dd>{this.state.price} {symbolInfo.currency}</dd>
-                <dt>Description</dt>
-                <dd>{description}</dd>
-            </dl>
-        );
+  renderSymbolInfo() {
+    let {
+      symbols,
+      activeSymbol,
+      company: { description }
+    } = this.state;
+    if (!symbols.length || !activeSymbol) {
+      return null;
     }
+    const regex = new RegExp(`^${activeSymbol}`, "i");
+    let symbolInfo = symbols.find(item => regex.test(item.symbol));
+    if (!symbolInfo) {
+      return null;
+    }
+    return (
+      <dl>
+        <dt>Symbol</dt>
+        <dd>{activeSymbol}</dd>
+        <dt>Current Stock Price</dt>
+        <dd>
+          {this.state.price} {symbolInfo.currency}
+        </dd>
+        <dt>Description</dt>
+        <dd>{description}</dd>
+      </dl>
+    );
+  }
 
-    render () {
-        const items = this.state.showAutocomplete ? this.state.symbols.map(item => item.symbol) : [];
-        const options = this.state.symbols.map(item => ({ value: item.symbol, label: item.symbol }));
-        return (
-            <div className="my-iex_info_lookup">
-                <Switch
-                    value={this.state.showAutocomplete}
-                    onSwitchToggle={this.handleSwitchToggle}
-                />
-                {this.state.showAutocomplete && <Autocomplete
-                    items={items}
-                    placeholder="Start to type..."
-                    value={this.state.activeSymbol}
-                    onValueUpdated={this.handleSymbolChanged}
-                />}
-                <WindowedSelect
-                    className={"my-iex_info_lookup-autocomplete " + (this.state.showAutocomplete ? 'hide' : '')}
-                    filterOption={createFilter({ ignoreAccents: false })}
-                    isLoading={!options.length}
-                    options={options}
-                    value={options.find(option => option.value === this.state.activeSymbol)}
-                    onChange={(payload) => this.handleSymbolChanged(payload.value)}
-                />
-                {this.renderSymbolInfo()}
-            </div>
-        );
-    }
+  render() {
+    const items =
+      true || this.state.showAutocomplete
+        ? this.state.symbols.map(item => item.symbol)
+        : [];
+    const options = this.state.symbols.map(item => ({
+      value: item.symbol,
+      label: item.symbol
+    }));
+    return (
+      <div className="my-iex_info_lookup">
+        <Switch
+          value={this.state.showAutocomplete}
+          onSwitchToggle={this.handleSwitchToggle}
+        />
+        <Autocomplete
+          className={this.state.showAutocomplete ? "" : "hide"}
+          items={items}
+          placeholder="Start to type..."
+          value={this.state.activeSymbol}
+          onValueUpdated={this.handleSymbolChanged}
+        />
+        <WindowedSelect
+          className={
+            "my-iex_info_lookup-autocomplete " +
+            (this.state.showAutocomplete ? "hide" : "")
+          }
+          filterOption={createFilter({ ignoreAccents: false })}
+          isLoading={!options.length}
+          options={options}
+          value={options.find(
+            option => option.value === this.state.activeSymbol
+          )}
+          onChange={payload => this.handleSymbolChanged(payload.value)}
+        />
+        {this.renderSymbolInfo()}
+      </div>
+    );
+  }
 }
 
-export default IEXInfoLookup
+export default IEXInfoLookup;
